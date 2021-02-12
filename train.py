de9fb9ce774cbde8a42f5d737644ca2b1d3c50f2
Gunisha Chaturvedi <gunisha3007@gmail.com>
2021-02-12 19:29:38 +0000

    Add files via upload

--- train.py
+++ train.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 from sklearn.linear_model import LinearRegression
 import argparse
 import os
@@ -15,17 +9,9 @@ import pandas as pd
 from azureml.core.run import Run
 from azureml.core import Dataset
 
-
-# In[ ]:
-
-
 run = Run.get_context()
 ws = run.experiment.workspace
 ds = ws.datasets['housedata']
-
-
-# In[ ]:
-
 
 def clean_data(data):
     dCol = ['id', 'date', 'zipcode', 'waterfront', 'yr_renovated']
@@ -33,16 +19,12 @@ def clean_data(data):
     x, y = data.drop('price', axis=1), data['price']
     return x,y
 
-
-# In[ ]:
-
-
 def main():
     # Add arguments to script
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('lrate', type=float, default=1.0, help="Learning rate")
-    parser.add_argument('niter', type=int, default=100, help="Number of iterations")
+    parser.add_argument('--lrate', type=float, default=1.0, help="Learning rate")
+    parser.add_argument('--niter', type=int, default=100, help="Number of iterations")
 
     args = parser.parse_args()
 
@@ -54,13 +36,15 @@ def main():
     x_train = st.fit_transform(x_train)
     x_test = st.transform(x_test)
     model = LinearRegression(lrate=args.lrate, niter=args.niter).fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+    mse= mean_squared_error (y,y_pred)
+    run.log("MSE", np.float(mse))
 
-    accuracy = model.score(x_test, y_test)
-    run.log("Accuracy", np.float(accuracy))
+#     accuracy = model.score(x_test, y_test)
+#     run.log("Accuracy", np.float(accuracy))
 
     os.makedirs('outputs', exist_ok=True)
     joblib.dump(model, 'outputs/project_model.joblib')
 
 if __name__ == '__main__':
     main()
-
